3fc219fe80ed86494ddf68e058b9eefbad32b0e2
sonomastea <sbstea@usf.edu>
2024-06-18 15:26:52 -0400

    Implement Data loading and DOM manipulation with D3.js

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
 
 //Function to load data from CSV file and display it
 function loadandDisplayData(){
-    d3.dsv(",", "data/purchase_orders.csv", d3.autoType).then(data=>{
-        const ul= d3.select("#purchse-orders-list");
+    d3.dsv(",","data/purchase_orders.csv" d3.autoType).then(data=>{
+        const ul= d3.select("#purchase-orders-list");
         data.forEach(order =>{
             ul.append("li")
-            .text('${order.customerName - Order ID: ${order.orderID} - Purchase Amount: ${order.purchaseAmount}');
+            .text('${order.customerName ,Order ID: ${order.orderID}  ,Purchase Amount: ${order.purchaseAmount}');
 
         })
         .catch(error =>{
