b0b927b9b2c00b7d4c60253a15c4efd14b119439
sonomastea <sbstea@usf.edu>
2024-06-19 20:46:09 -0400

    finalize

--- example.js
+++ example.js
@@ -2,15 +2,15 @@
 //Function to load data from CSV file and display it
 function loadandDisplayData(){
     d3.dsv(",","data/purchase_orders.csv", d3.autoType).then(data=>{
-        const ul= d3.select("#purchase-orders-list");
-        data.forEach(order =>{
-            ul.append("li")
-            .text('${order.customerName ,Order ID: ${order.orderID}  ,Purchase Amount: ${order.purchaseAmount}');
+        const ordersList= d3.select("#purchase-orders-list");
+        .data(data)
+        .enter()
+        .append("li")
+            .text(function)(d)('${order.customerName ,Order ID: ${order.orderID}  ,Purchase Amount: ${order.purchaseAmount}');
 
         })
         .catch(error =>{
             console.error("Error loading CSV file:", error);
             
         })
-    })
-}
+    }
